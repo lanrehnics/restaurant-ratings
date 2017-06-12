7339dc0ef7e1c4461f5d27f48bef5e36b9e9fba8
Abel Gaxiola <abel.gaxiola@pima.gov>
2017-06-12 15:02:33 -0700

    Modify for restaurant ratings app.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  restaurants = [
+    { id: 1, name: 'Mezon del Cobre' },
+    { id: 2, name: 'Gentle Ben\'s' },
+    { id: 3, name: 'The Golden Arches' }
+  ];
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  viewDetails(id:number) {
+    alert(id);
+  }
 }
