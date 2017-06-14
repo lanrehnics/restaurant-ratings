5294ac1f5b20e1363d8ae8abb767a264d0e32ead
Abel Gaxiola <abel.gaxiola@pima.gov>
2017-06-14 07:32:40 -0700

    Remove five-stars image, replaced with icons.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,7 +9,8 @@ export class HomePage {
   restaurants = [
     { id: 1, name: 'Mezon del Cobre' },
     { id: 2, name: 'Gentle Ben\'s' },
-    { id: 3, name: 'The Golden Arches' }
+    { id: 3, name: 'The Golden Arches' },
+    { id: 3, name: 'Bamboo Palace' }
   ];
 
   constructor(public navCtrl: NavController) {
